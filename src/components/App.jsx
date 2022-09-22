1c6cabaef6b1e418321a31eeff0a15023eedb677
unknown <miguel.semedo.2@outlook.com>
2022-09-22 13:17:34 +0100

    implement add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
+	const [notes, setNotes] = useState([]);
+
+	function addNote(newNote) {
+		setNotes((prevNotes) => {
+			return [...prevNotes, newNote];
+		});
+	}
+
 	return (
 		<div>
 			<Header />
-			{notes.map((note) => (
-				<Note key={note.key} title={note.title} content={note.content} />
-			))}
+			<CreateArea onAdd={addNote} />
+			{notes.map((noteItem) => {
+				return <Note title={noteItem.title} content={noteItem.content} />;
+			})}
 			<Footer />
 		</div>
 	);
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function CreateArea(props) {
+	const [note, setNote] = useState({
+		title: '',
+		content: '',
+	});
+
+	function handleChange(e) {
+		const { name, value } = e.target;
+		setNote((prevNote) => {
+			return {
+				...prevNote,
+				[name]: value,
+			};
+		});
+	}
+
+	function submitNote(e) {
+		props.onAdd(note);
+
+		e.preventDefault();
+	}
+
+	return (
+		<div>
+			<form>
+				<input
+					onChange={handleChange}
+					name="title"
+					type="text"
+					placeholder="Title"
+					value={note.title}
+				></input>
+				<textarea
+					onChange={handleChange}
+					name="content"
+					type="text"
+					placeholder="Take a note..."
+					rows="3"
+					value={note.content}
+				></textarea>
+				<button onClick={submitNote}>Add</button>
+			</form>
+		</div>
+	);
+}
+
+export default CreateArea;
